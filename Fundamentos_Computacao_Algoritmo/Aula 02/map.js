8fb1e615a15fbc6c56e9597fccde847945d9c514
xrafaelmuller <xrafaelmuller@outlook.com>
2024-09-17 13:33:22 -0300

    set and map

---
+++ Fundamentos_Computacao_Algoritmo/Aula 02/map.js
@@ -0,0 +1,12 @@
+var novoMapa = new Map();
+
+novoMapa.set('ue',{});
+novoMapa.set('ue',{});
+novoMapa.set('ue',{});
+novoMapa.set('ue',{});
+novoMapa.set('ue',{});
+novoMapa.set('ue',{})
+
+
+var resultado = novoMapa;
+console.log(resultado); novoMapa
---
+++ Fundamentos_Computacao_Algoritmo/Aula 02/set.js
@@ -0,0 +1,7 @@
+var novoSet = new Set();
+
+novoSet.add('back');
+novoSet.add('front');
+
+var resultado = novoSet;
+console.log(resultado); novoSet
